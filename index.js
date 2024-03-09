8c6dad8bea8299b0c731538779d4ce7c23bdb13e
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-03-09 16:25:25 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const ytdl = require("ytdl-core");
 const cors = require("cors");
+const ytcm = require("@freetube/yt-comment-scraper")
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,20 +11,16 @@ app.use(cors());
 // Define an endpoint to fetch the raw video
 app.get("/video", async (req, res) => {
   try {
-    const videoUrl = "https://youtube.com/watch?v=" + req.query.id; // The YouTube video URL should be provided as a query parameter
+    const videoUrl = "https://youtube.com/watch?v=" + req.query.id;
     if (!videoUrl) {
       return res.status(400).json({ error: "Video URL is required" });
     }
 
-    // Check if the URL is a valid YouTube URL
     if (!ytdl.validateURL(videoUrl)) {
       return res.status(400).json({ error: "Invalid YouTube URL" });
     }
 
-    // Get video information
     const info = await ytdl.getInfo(videoUrl);
-
-    console.log(info.player_response.streamingData);
 
     res.json({
       "video-url": info.player_response.streamingData.formats[0].url,
@@ -33,26 +31,24 @@ app.get("/video", async (req, res) => {
   }
 });
 
+// Define an endpoint to get video information
 app.get("/video/info", async (req, res) => {
   try {
-    const videoUrl = "https://youtube.com/watch?v=" + req.query.id; // The YouTube video URL should be provided as a query parameter
+    const videoUrl = "https://youtube.com/watch?v=" + req.query.id;
     if (!videoUrl) {
       return res.status(400).json({ error: "Video URL is required" });
     }
 
-    // Check if the URL is a valid YouTube URL
     if (!ytdl.validateURL(videoUrl)) {
       return res.status(400).json({ error: "Invalid YouTube URL" });
     }
 
-    // Get video information
     const info = await ytdl.getInfo(videoUrl);
 
-    // Extracting only required information
     const videoInfo = {
       title: info.videoDetails.title,
       description: info.videoDetails.description,
-      thumbnail_url: info.videoDetails.thumbnail.thumbnails[0].url, // Assuming you want the first thumbnail
+      thumbnail_url: info.videoDetails.thumbnail.thumbnails[0].url,
     };
 
     res.json(videoInfo);
@@ -65,21 +61,36 @@ app.get("/video/info", async (req, res) => {
 // Define an endpoint to stream the video
 app.get("/video/stream", async (req, res) => {
   try {
-    const videoUrl = "https://youtube.com/watch?v=" + req.query.id; // The YouTube video URL should be provided as a query parameter
+    const videoUrl = "https://youtube.com/watch?v=" + req.query.id;
     if (!videoUrl) {
       return res.status(400).json({ error: "Video URL is required" });
     }
 
-    // Check if the URL is a valid YouTube URL
     if (!ytdl.validateURL(videoUrl)) {
       return res.status(400).json({ error: "Invalid YouTube URL" });
     }
 
-    // Set the response header to serve video content
     res.header("Content-Type", "video/mp4");
-
-    // Pipe the video stream to the response
     ytdl(videoUrl, { format: "mp4" }).pipe(res);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+// Define an endpoint to fetch video comments
+app.get("/video/comments", async (req, res) => {
+  try {
+    const payload = {
+      videoId: req.query.id, // Required
+      sortByNewest: "sortByNewest"
+    }
+
+    ytcm.getComments(payload).then((data) =>{
+        res.json(data)
+    }).catch((error)=>{
+        console.log(error);
+    });
   } catch (error) {
     console.error("Error:", error);
     res.status(500).json({ error: "Something went wrong" });
