4e2723def0e8282ea8d69b8083b5181942ee5eda
Gen1x-ALT <153012779+Gen1x-ALT@users.noreply.github.com>
2024-03-09 13:11:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const ytdl = require("ytdl-core");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+// Define an endpoint to fetch the raw video
+app.get("/video", async (req, res) => {
+  try {
+    const videoUrl = "https://youtube.com/watch?v=" + req.query.id; // The YouTube video URL should be provided as a query parameter
+    if (!videoUrl) {
+      return res.status(400).json({ error: "Video URL is required" });
+    }
+
+    // Check if the URL is a valid YouTube URL
+    if (!ytdl.validateURL(videoUrl)) {
+      return res.status(400).json({ error: "Invalid YouTube URL" });
+    }
+
+    // Get video information
+    const info = await ytdl.getInfo(videoUrl);
+
+    console.log(info.player_response.streamingData)
+
+    res.json({"video-url": info.player_response.streamingData.formats[0].url})
+    //res.send("<script>console.log(" + JSON.stringify(info.player_response.streamingData) + ")</script>")
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
